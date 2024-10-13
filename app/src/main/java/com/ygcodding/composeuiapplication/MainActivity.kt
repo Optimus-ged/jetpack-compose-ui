63fd316a68c363f6148a670dc8ad8dc889396cb7
Optimus ged <gedeonyalakuhanda@gmail.com>
2024-10-13 13:45:27 +0200

    small basic calculator created

--- app/src/main/java/com/ygcodding/composeuiapplication/MainActivity.kt
+++ app/src/main/java/com/ygcodding/composeuiapplication/MainActivity.kt
@@ -7,15 +7,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ygcodding.composeuiapplication.ui.theme.ComposeUiApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,21 +32,75 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeUiApplicationTheme {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Text(text = "Yals is a dev")
-                    Text(text = "Hello there")
-                    Button(
-                        onClick = {
-                            Toast.makeText(applicationContext, "Hey bro hos is it there?", Toast.LENGTH_SHORT).show()
-                        }
-                    ) {
-                        Text(text ="Click here bro")
-                    }
+                var num1 by remember {
+                    mutableStateOf("0")
                 }
+                var num2 by remember {
+                    mutableStateOf("0")
+                }
+
+               Column {
+                   Text(text = "basic calcuculator", modifier = Modifier.padding(start = 20.dp, top = 50.dp))
+                   TextField(
+                       value = num1,
+                       onValueChange = {
+                           num1=it
+                       }
+                   )
+                   TextField(
+                       value = num2,
+                       onValueChange = {
+                           num2=it
+                       }
+                   )
+                   Row(
+                       modifier = Modifier.padding(top = 50.dp)
+                   ) {
+                       Button(
+                           onClick = {
+                               val result = num1.toInt() + num2.toInt()
+                               Toast.makeText(applicationContext, "The result is $result", Toast.LENGTH_SHORT).show()
+                           }
+                       ) {
+                           Text("+")
+                       }
+                       Spacer(modifier = Modifier.padding(end = 20.dp))
+                       Button(
+                           onClick = {
+                               var result = 0
+                               if(num1.toInt() > num2.toInt()){
+                                  result = num1.toInt() - num2.toInt()
+                                   Toast.makeText(applicationContext, "The result is $result", Toast.LENGTH_SHORT).show()
+                               }
+
+                           }
+                       ) {
+                           Text("-")
+                       }
+                       Spacer(modifier = Modifier.padding(end = 20.dp))
+                       Button(
+                           onClick = {
+                               val result = num1.toInt() * num2.toInt()
+                               Toast.makeText(applicationContext, "The result is $result", Toast.LENGTH_SHORT).show()
+                           }
+                       ) {
+                           Text("*")
+                       }
+                       Spacer(modifier = Modifier.padding(end = 20.dp))
+                       Button(
+                           onClick = {
+                               var result = 0
+                               if(num1.toInt() > num2.toInt()){
+                                   result = num1.toInt() / num2.toInt()
+                                   Toast.makeText(applicationContext, "The result is $result", Toast.LENGTH_SHORT).show()
+                               }
+
+                           }
+                       ) {
+                           Text("/")
+                       }
+                   }
+               }
             }
         }
     }
